7f01598270c5b03b09a8cd5100ee4ec4e8f4dde9
Sreedhar <vrudhulasreedhar@gmail.com>
2024-10-07 00:17:10 -0400

    Updated API endpoints using Express Mongo

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,42 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try{
+        const documents = await Dealerships.find();
+        res.json(documents);
+    }
+    catch(error){
+        res.status(500).json({ error: 'Error fetching documents'});
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+  let state = req.params.state;
+  try{
+    const documents = await Dealerships.find({state: state});
+    res.json(documents);
+}
+catch(error){
+    res.status(500).json({ error: 'Error fetching documents'});
+}
+
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+
+    let dealer_id = Number(req.params.id);
+    try{
+        const documents = await Dealerships.find({ id: dealer_id});
+        res.json(documents); 
+        // console.log("Requested ID:", dealer_id);
+    }
+    catch(error)
+    {
+        res.status(500).json({ error: 'Error fetching documents'});
+    }
+
 });
 
 //Express route to insert review
