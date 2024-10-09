700571454b881e1ed0e7a065016c851c720aef91
Sreedhar <vrudhulasreedhar@gmail.com>
2024-10-09 07:01:57 +0530

    Update admin.py
    resolved linting errors

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,17 +1,13 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-# Register your models here.
-
 # CarModelInline class
-
 class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 2
     readonly_fields = ['manufacture_type']
     
 # CarModelAdmin class
-
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ['car_make', 'name', 'type', 'year']
     list_filter = ['car_make']
@@ -20,14 +16,12 @@ class CarModelAdmin(admin.ModelAdmin):
     search_fields = ['name', 'car_make__name']
 
 # CarMakeAdmin class with CarModelInline
-
 class CarMakeAdmin(admin.ModelAdmin):
     fields = ['name', 'description']
     search_fields = ['name']
     # now adding inline claases(i.e. one to many class)
     inlines = [CarModelInline]
-
+    
 # Register models here
-
 admin.site.register(CarModel, CarModelAdmin)
 admin.site.register(CarMake, CarMakeAdmin)
