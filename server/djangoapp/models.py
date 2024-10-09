63c6224e171a8028ec4d12ba98e2c5b3afa6cdab
Sreedhar <vrudhulasreedhar@gmail.com>
2024-10-09 07:09:56 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -19,6 +19,7 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+
 class CarModel(models.Model):
     name = models.CharField(max_length=100)
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
@@ -30,12 +31,13 @@ class CarModel(models.Model):
         ('AWD', 'AWD'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2024,validators=[
+    year = models.IntegerField(default=2024, validators=[
             MaxValueValidator(2024),
             MinValueValidator(2015)
         ])
-    MANUFACTURE_TYPES = [('LOCAL','LOCAL'),('IMPORTED','IMPORTED')]
-    manufacture_type = models.CharField(max_length=10, choices=MANUFACTURE_TYPES,default='LOCAL')
+    MANUFACTURE_TYPES = [('LOCAL', 'LOCAL'), ('IMPORTED', 'IMPORTED')]
+    manufacture_type = models.CharField(max_length=10, choices=MANUFACTURE_TYPES, 
+                                        default='LOCAL')
 
     def __str__(self):
         return self.name
